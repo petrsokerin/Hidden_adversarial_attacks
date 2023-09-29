f2e2d3081a2d5de499e09da2dbbd37f9ccc99751
Petr Sokerin <sokerinpo@mail.ru>
2023-09-29 12:17:30 +0000

    modification attack

--- attack_run.py
+++ attack_run.py
@@ -14,12 +14,9 @@ from torch.utils.data import DataLoader
 from utils.data import load_Ford_A, transform_data, MyDataset
 from models.models import LSTM_net
 
-from utils.attacks import ifgsm_procedure
+from utils.attacks import attack_procedure
 from utils.utils import save_experiment, load_disc_model
-from utils.discrim_training import HideAttackExp
 from utils.config import get_attack, load_disc_config
-from utils.attacks import (fgsm_disc_attack, fgsm_attack, fgsm_reg_attack, 
-simba_binary, simba_binary_reg, simba_binary_disc_reg)
 from utils.TS2Vec.datautils import load_UCR
 
 
@@ -47,17 +44,21 @@ def main(cfg: DictConfig):
 
 
     model = instantiate(cfg.attack_model).to(device)
+    
     disc_model = instantiate(cfg.disc_model).to(device)
     
-    disc_model_check = instantiate(cfg.disc_model_check).to(device)
-    disc_model_check = load_disc_model(
-        copy.deepcopy(disc_model_check),
-        model_id=cfg['disc_check_params']['model_id'], 
-        path=cfg['disc_path'], 
-        model_name=cfg['disc_check_params']['model_name'], 
-        device=device
-        )
-    disc_model_check.eval()
+    if cfg['use_disc_check']:
+        disc_model_check = instantiate(cfg.disc_model_check).to(device)
+        disc_model_check = load_disc_model(
+            copy.deepcopy(disc_model_check),
+            model_id=cfg['disc_check_params']['model_id'], 
+            path=cfg['disc_path'], 
+            model_name=cfg['disc_check_params']['model_name'], 
+            device=device,
+            )
+        disc_model_check.eval()
+    else: 
+        disc_model_check = None
 
     alphas = [0]
     if 'reg' in cfg['attack_type'] or 'disc' in cfg['attack_type']:
@@ -84,7 +85,7 @@ def main(cfg: DictConfig):
         model_path = cfg['model_folder'] + f'model_{cfg["model_id_attack"]}_{cfg["dataset"]}.pth'
         model.load_state_dict(copy.deepcopy(torch.load(model_path)))
 
-        aa_res_df, rej_curves_dict = ifgsm_procedure(model=model, loader=test_loader, criterion=criterion,
+        aa_res_df, rej_curves_dict = attack_procedure(model=model, loader=test_loader, criterion=criterion,
                                                     attack_func=attack_func, attack_params=attack_params,
                                                     all_eps=cfg['all_eps'], n_steps=cfg['n_iterations'],
                                                     n_objects=n_objects, train_mode=cfg['train_mode'],
