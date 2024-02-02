cac85b74afcb389a78c68d123fc0afec4a49f226
petrsokerin <petya4343@mail.ru>
2024-02-02 10:48:06 +0000

    debug configs models problem

--- attack_run.py
+++ attack_run.py
@@ -16,14 +16,15 @@ from src.attacks import attack_procedure
 from src.utils import save_experiment, load_disc_model
 from src.config import get_attack, load_disc_config
 
-CONFIG_NAME = 'attack_deepfool'
+CONFIG_NAME = 'attack_run_config'
 
 @hydra.main(config_path='config/my_configs', config_name=CONFIG_NAME, version_base=None)
 def main(cfg: DictConfig):
 
     if cfg['test_run']:
         print('ATTENTION!!!! Results will not be saved. Set param test_run=False')
-    # load data
+    # load data  
+    print(cfg['dataset'])
     X_train, y_train, X_test, y_test = load_data(cfg['dataset'])
     X_train, X_test, y_train, y_test = transform_data(X_train, X_test, y_train, y_test, slice_data=cfg['slice'])
   
