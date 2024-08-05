c32eabc77b735b4f49a87f2713a47382d1f4c44c
Dmitry Anikin <mtmnt1@mail.ru>
2024-08-05 11:41:45 +0300

    rename fix

--- src/attacks/simba.py
+++ src/attacks/simba.py
@@ -1,78 +1,89 @@
 import torch
 
 from .base_attacks import BaseIterativeAttack
-from .regularizers import reg_disc, reg_neigh
+from .regularizers import reg_disc, reg_neigh, reg_disc_no_attack
 
 
 class SimBABinary(BaseIterativeAttack):
     def __init__(self, model, criterion, eps, n_steps, device="cpu"):
         super().__init__(model, criterion, eps, n_steps, device=device)
 
-    def step(self, x, y_true):
-        rand_ind = torch.randint(x.shape[1], (1,))[0]
-        mask = torch.zeros(x.shape).to(self.device)
+    def get_loss(self, X: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        self.model.zero_grad()
+        y_pred = self.model(X)
+        loss = self.criterion(y_pred, y_true)
+        return loss
+
+    def generate_changes(X):
+        rand_ind = torch.randint(X.shape[1], (1,))[0]
+        mask = torch.zeros(X.shape).to(self.device)
         mask[:, rand_ind] = mask[:, rand_ind] + 1
 
-        x_minus = x - mask * self.eps
-        x_plus = x + mask * self.eps
+        X_minus = X - mask * self.eps
+        X_plus = X + mask * self.eps
 
-        y_pred = self.model(x)
-        y_pred_minus = self.model(x_minus)
-        y_pred_plus = self.model(x_plus)
+        return X_minus, X_plus
 
-        p_orig = torch.sigmoid(y_pred)
-        p_minus = torch.sigmoid(y_pred_minus)
-        p_plus = torch.sigmoid(y_pred_plus)
 
-        x_all = torch.cat([x, x_minus, x_plus]).view(3, *x.shape)
+    def step(self, X, y_true):
+        X_minus, X_plus = self.generate_changes(X)
+
+        y_pred = self.model(X)
+        y_pred_minus = self.model(X_minus)
+        y_pred_plus = self.model(X_plus)
+
+        X_all = torch.cat([X, X_minus, X_plus]).view(3, *X.shape)
         losses = torch.stack(
             [
-                self.criterion(p_orig, y_true),
-                self.criterion(p_minus, y_true),
-                self.criterion(p_plus, y_true),
+                self.criterion(y_pred, y_true),
+                self.criterion(y_pred_minus, y_true),
+                self.criterion(y_pred_plus, y_true),
             ],
             dim=0,
         )
 
         max_loss_indices = torch.argmax(losses, dim=0)
         max_loss_indices = (
-            min_loss_indices.unsqueeze(-1).unsqueeze(-1).expand(-1, x.shape[-1])
+        max_loss_indices.unsqueeze(-1).unsqueeze(-1).expand(-1, x.shape[-1])
         )
 
-        x_adv = torch.gather(x_all, 0, max_loss_indices).squeeze(0)
-        return x_adv
-
-
-class SimBABinaryReg(SimBABinary):
-    def __init__(self, model, criterion, eps, n_steps, alpha, device="cpu"):
-        super().__init__(model, criterion, eps, n_steps, device=device)
-        self.alpha = alpha
-
-    def step(self, x, y_true):
-        x_adv = super().step(x, y_true)
-        reg_value = reg_neigh(x_adv, self.alpha)
-
-        loss = -reg_value
-        grad_ = torch.autograd.grad(loss, x_adv, retain_graph=True)[0]
-        x_adv = x_adv.data + grad_
-
-        return x_adv
+        X_adv = torch.gather(X_all, 0, max_loss_indices).squeeze(0)
+        return X_adv
 
 
 class SimBABinaryDiscReg(SimBABinary):
     def __init__(
-        self, model, criterion, eps, n_steps, alpha, disc_models, device="cpu"
+        self, model, criterion, eps, n_steps, alpha, disc_models, device="cpu", use_sigmoid=False,
     ):
         super().__init__(model, criterion, eps, n_steps, device=device)
+        self.use_sigmoid = use_sigmoid
         self.alpha = alpha
         self.disc_models = disc_models
 
-    def step(self, x, y_true):
-        x_adv = super().step(x, y_true)
-        reg_value = reg_disc(x_adv, self.alpha, self.disc_models)
+    def step(self, X, y_true):
+        X_minus, X_plus = self.generate_random_changes(X)
 
-        loss = -reg_value
-        grad_ = torch.autograd.grad(loss, x_adv, retain_graph=True)[0]
-        x_adv = x_adv.data + grad_
+        y_pred = self.model(X)
+        y_pred_minus = self.model(X_minus)
+        y_pred_plus = self.model(X_plus)
 
-        return x_adv
+        X_all = torch.cat([X, X_minus, X_plus]).view(3, *X.shape)
+        losses = torch.stack(
+            [
+                self.criterion(y_pred, y_true) - self.alpha * reg_disc(X, self.disc_models, self.use_sigmoid),
+                self.criterion(y_pred_minus, y_true) - self.alpha * reg_disc(X_minus, self.disc_models, self.use_sigmoid),
+                self.criterion(y_pred_plus, y_true) - self.alpha * reg_disc(X_plus, self.disc_models, self.use_sigmoid),
+            ],
+            dim=0,
+        )
+
+        max_loss_indices = torch.argmax(losses, dim=0)
+        max_loss_indices = (
+            max_loss_indices.unsqueeze(-1).unsqueeze(-1).expand(-1, x.shape[-1])
+        )
+
+        X_adv = torch.gather(X_all, 0, max_loss_indices).squeeze(0)
+        return X_adv
+
+def govn():
+    pass
