2889625dfba5aa525b30ff3f0f9b859355bcee57
SophieKre <krehovasofia@gmail.com>
2024-11-11 06:47:14 +0000

    some fix

--- src/config/config.py
+++ src/config/config.py
@@ -126,13 +126,20 @@ def get_disc_list(
             weight_name = model_conf
             model_folder_name = model_conf
         disc_path = f"{path}/{model_folder_name}/{weight_name}.pt"
+        folder_path = 'loaded_clearml/disc_weights'
+        file_path = os.path.join(folder_path, weight_name)
+        if from_clearml and os.path.exists(file_path):
+            loaded_path = file_path
+        else:
+            loaded_path = weights_from_clearml_by_name(path, f"{weight_name}", load_weights='disc_weights')
+
 
         if from_clearml:
             disc = get_model(
             model_name,
             model_params,
             device=device,
-            path=weights_from_clearml_by_name(path, f"{weight_name}", load_weights='disc_weights'),
+            path=loaded_path,
             train_mode=train_mode,
         )
         else:
