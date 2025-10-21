7811917877d44942a661e0df47cdc4d2cf15fd49
baronovM <baronov05@gmail.com>
2025-10-21 09:33:02 +0300

    Integrate LSTM-based attack into pipeline

---
+++ src/attacks/trainable_attack.py
@@ -0,0 +1,28 @@
+from abc import ABC
+
+from torch import nn
+
+
+class Activation(nn.Module):
+    def __init__(self, kind='identity'):
+        super().__init__()
+        if kind == 'identity':
+            self.act = nn.Identity()
+        elif kind == 'relu':
+            self.act = nn.ReLU()
+        elif kind == 'tanh':
+            self.act = nn.Tanh()
+        else:
+            raise ValueError(f'Unknown activation {kind}')
+
+    def forward(self, x):
+        return self.act(x)
+
+
+class TrainableAttack:
+    def train_attack(self, train_loader, val_loader=None):
+        raise NotImplementedError
+
+    @property
+    def is_trainable(self):
+        return True
